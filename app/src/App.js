59d2c1efe899e0ede702150046119e079c36b464
Geoff Davis <gdavis@archermalmo.com>
2016-08-23 13:10:17 -0500

    init project. SPLIT HERE to start CRApS development

---
+++ app/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './assets/images/logo.svg';
+import './css/components/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <main className="App">
+      </main>
+    );
+  }
+}
+
+export default App;
