4af3c542a696b75e1e894b7cd15343f4db9b29e0
Geoff Davis <geoffdavis92@gmail.com>
2016-11-24 19:58:32 -0600

    add API data/PHP files

--- pages/api/fn.php
+++ pages/api/fn.php
@@ -4,15 +4,4 @@ function data( $filename ) {
 	return "../data/$filename";
 }
 
-function readFileContents( $filename/*, $callback*/ ) {
-	if ( file_exists($filename) ) {
-		$file = fopen($filename, 'r');
-		$contents = fread($file, filesize($filename));
-		fclose($file);
-		return $contents;
-	} else {
-		return false;
-	}
-}
-
 ?>
