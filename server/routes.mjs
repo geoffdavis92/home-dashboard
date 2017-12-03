b08db6d4637181b86384608ead455482422b2e68
Geoff Davis <geoffdavis92@gmail.com>
2017-12-03 03:00:10 -0600

    update routes, bundle

--- server/routes.mjs
+++ server/routes.mjs
@@ -3,7 +3,7 @@ import { routeLogger } from "./middleware/index";
 
 const BaseRouter = express.Router();
 
-BaseRouter.get("/", routeLogger, (req, res) => {
+BaseRouter.get(["/", "/dashboard"], routeLogger, (req, res) => {
 	res.render("index", {
 		fn: {
 			addStyles: function addStyles() {
